c44f617bc457d214e9f9db0a5c0ce07c141aab58
emsfuesler <60246825+emsfuesler@users.noreply.github.com>
2020-11-22 18:04:49 -0800

    wtf

---
+++ javatutorial/main.js
@@ -0,0 +1,23 @@
+var stars = 0;
+
+$(document).ready(function () {
+
+$(".scene").hide();
+$("#s1").show();
+
+$("#option1").click(function(){
+  $("#s1").hide();
+  $("#s2").show();
+
+  stars ++ ;
+  $("#stars").html(stars);
+})
+
+
+  $("#option2").click(function(){
+    $("#s1").hide();
+    $("#s3").show();
+
+});
+
+});
